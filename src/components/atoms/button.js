1dd754d06394c95da28e4baa6f13cfc191bab439
suicide11 <b117045@iiit-bh.ac.in>
2020-05-07 16:45:09 +0530

    done some small fix

--- src/components/atoms/button.js
+++ src/components/atoms/button.js
@@ -3,16 +3,16 @@ import { Button } from 'antd';
 function CustomButton(props) {
     const style={
         boxShadow:"0 2px 0 rgba(0, 0, 0, 0.045)",
-        backgroundColor:"#1890ff",
-        border:"solid #1890ff 1px",
-        color:"white",
+        backgroundColor:props.data.backgroundColor,
+        border:"solid"+ props.data.backgroundColor +"1px",
+        color:props.data.font_color,
         height:"100%",
         width:"100%",
         // borderColor:"#1890ff"
     }
     return(
         <Button type="primary" style={style} block >
-            hello
+            {props.data.value}
         </Button>
     )
 }
